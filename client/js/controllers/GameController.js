29d96b967e217175f7e43712b7a94a6a3fecb4ec
LielKaysari <elixir03@gmail.com>
2018-05-22 10:39:17 +0300

    damn

--- client/js/controllers/GameController.js
+++ client/js/controllers/GameController.js
@@ -19,14 +19,14 @@ let port;
 
 /** Audio handling */
 window.audioHandler = audioHandler;
-// fetch("http://localhost/aport")
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(myJson) {
-//     port = myJson;
-//   });
-// console.log(process.env);
+fetch("http://localhost/aport")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    port = myJson;
+  });
+console.log(port);
 document.addEventListener("keydown", event => {
   if (event.keyCode === Config.SOUND_TOGGLE) {
     audioHandler();
